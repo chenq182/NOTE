bd7c4bc9fd57b76a1afa1904cdac1f42750b30de
Quan Chen <chenq182@sina.com>
2017-07-19 11:12:59 +0800

    Update venv.sh

--- python/venv.sh
+++ python/venv.sh
@@ -5,5 +5,6 @@ sudo apt-get install python3-venv  ## optional
 python3 -m venv ~/myvenv
 
 source ~/myvenv/bin/activate
-pip3 freeze
+pip3 freeze > ~/requirements.txt
+pip3 install -r ~/requirements.txt
 deactivate
