46926826bfe311840ce26545578dcb6bf7bae320
Quan Chen <chenq182@sina.com>
2017-10-10 15:13:03 +0800

    2017-10-10 15:00:00

--- maven/webapp.sh
+++ maven/webapp.sh
@@ -3,3 +3,6 @@ mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -Darchetyp
 mvn package
 mvn idea:idea
 mvn eclipse:eclipse
+
+mvn dependency:copy-dependencies
+mvn dependency:tree > mvnTree.txt
