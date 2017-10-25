14b83a293cc2b269a1818939beeb8d8787d6b3cf
Quan Chen <chenq182@sina.com>
2017-10-25 10:40:56 +0800

    2017-10-25 10:40:00

---
+++ linux/vim.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o nounset
+cd $(dirname $0)
+
+##################################################
+# Common functions and variables
+##################################################
+
+source .utils/common_functions
+
+##################################################
+# Script body
+##################################################
+
+Step "Check for root privileges"
+    if [[ "$UID" != 0 ]]; then
+        Error "You don't have root privileges. Please use sudo."
+        exit 1
+    fi
+Done
+
+Step "Update packages"
+    apt-get update
+Done
+
+Step "Upgrade packages"
+    apt-get upgrade
+Done
+
+Step "Vimrc configuration"
+    echo -e "set nu\nset noai nosi" > ~/.vimrc
+Done
+
+Message "FINISHED."
+
+# vim: set tabstop=4 shiftwidth=4:
