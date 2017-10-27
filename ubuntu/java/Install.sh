2f3cadfaf89df9b89e926d5adf8d59e52866d23a
Quan Chen <chenq182@sina.com>
2017-10-27 10:21:30 +0800

    2017-10-27 10:21:30

---
+++ ubuntu/java/Install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -o nounset
+cd $(dirname $0)
+
+##################################################
+# Ubuntu 16.04 specific settings
+##################################################
+
+DES_PATH="/usr/share"
+DES_NAME="java"
+PKG_URL="--no-check-certificate --no-cookies --header \"Cookie: oraclelicense=accept-securebackup-cookie\" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz"
+PKG_NAME="jdk1.8.0_152"
+
+InstallCFGs() {
+    Backup /etc/profile
+    sed -i '/# JAVA BEGIN #/{x;:a;N;/# JAVA END #/!ba;d}' /etc/profile
+    echo -e "\n# JAVA BEGIN #" >>/etc/profile
+    echo -e "export JAVA_HOME=${DES_PATH}/${DES_NAME}" >>/etc/profile
+    echo "# JAVA END #" >>/etc/profile
+    source /etc/profile
+
+    ln -s ${DES_PATH}/${DES_NAME}/bin/java /usr/local/bin
+    ln -s ${DES_PATH}/${DES_NAME}/bin/javac /usr/local/bin
+    ln -s ${DES_PATH}/${DES_NAME}/bin/javah /usr/local/bin
+}
+
+##################################################
+# Common functions and variables
+##################################################
+
+source ../.utils/common_functions
+
+##################################################
+# Script body
+##################################################
+
+Step "Check for root privileges"
+    if [[ "$UID" != 0 ]]; then
+        Error "You don't have root privileges. Please use sudo."
+        exit 1
+    fi
+Done
+
+Step "Downloading PKG"
+    PKG_FILE=${PKG_URL##*/}
+    if [ -z "$PKG_NAME" ];then
+        PKG_NAME=${PKG_FILE%%.tar*}
+    fi
+
+    if [ -f "${PKG_FILE}" ];then
+        Error "${PKG_FILE} exists."
+        exit 1
+    else
+        wget -c ${PKG_URL}
+    fi
+Done
+
+Step "Install PKG"
+    tar zxvf ${PKG_FILE} -C ${DES_PATH}/
+    echo -n "" >${PKG_FILE}
+    rm -rf ${DES_PATH}/${DES_NAME}
+    mv ${DES_PATH}/${PKG_NAME} ${DES_PATH}/${DES_NAME}
+Done
+
+Step "Install config files"
+    InstallCFGs
+Done
+
+Message "FINISHED."
+
+# vim: set tabstop=4 shiftwidth=4:
