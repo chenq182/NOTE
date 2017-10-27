9818e7f41219943e98adb56a0458a41a07692ccb
Quan Chen <chenq182@sina.com>
2017-10-27 09:05:33 +0800

    2017-10-27 09:05:33

--- git.sh
+++ git.sh
@@ -3,6 +3,9 @@
 set -o nounset
 # git clone https://github.com/chenq182/NOTE
 
+sed -i '/# Symbolic Links #/{p;:a;N;$!ba;d}' .gitignore
+find . -type l | sed -e s'/^\.\///g' >>.gitignore
+
 git add .
 git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push origin master
