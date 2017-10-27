476271d4363e49044a5b8aeca9bedea97b9b7dd1
Quan Chen <chenq182@sina.com>
2017-10-27 16:17:21 +0800

    2017-10-27 16:17:21

--- maven/webapp.sh
+++
@@ -1,8 +0,0 @@
-mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DgroupId=org.trimps.sample -DartifactId=sampleproject
-
-mvn package
-mvn idea:idea
-mvn eclipse:eclipse
-
-mvn dependency:copy-dependencies
-mvn dependency:tree > mvnTree.txt
---
+++ ubuntu/maven/note.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mvn package
+
+# mvn dependency:copy-dependencies
+# mvn dependency:tree > mvnTree.txt
+
