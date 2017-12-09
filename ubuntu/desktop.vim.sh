08dcca500b86b85caf0afd14d8208eb144ff5215
Quan Chen <chenq182@sina.com>
2017-12-09 18:45:03 +0800

    Update and rename grub.hidden.sh to desktop.vim.sh

--- ubuntu/desktop.vim.sh
+++ ubuntu/desktop.vim.sh
@@ -33,10 +33,4 @@ Step "Vim configuration"
     echo -e "set nu\nset noai nosi" > ~/.vimrc
 Done
 
-Step "Hide Grub"
-    Backup /etc/default/grub
-    sed -i 's/^.*GRUB_HIDDEN_TIMEOUT=.*$/GRUB_HIDDEN_TIMEOUT=0/g' /etc/default/grub
-    update-grub
-Done
-
 Message "FINISHED."
